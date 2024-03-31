5dbfb7440851ea2aedf2626f0a73fbd4c86ff4ab
RogersCameron <156271172+RogersCameron@users.noreply.github.com>
2024-03-31 17:50:23 -0400

    t

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-const express = require("express");
+const express = require('express');
+const detectPort = require('detect-port');
 const app = express();
-app.use(express.static("public"));
+
+app.use(express.static('public'));
 app.use(express.json());
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const PORT = 3000;
+
+detectPort(PORT, (err, availablePort) => {
+  if (err) {
+    console.error('Error detecting port:', err);
+    return;
+  }
+
+  if (PORT === availablePort) {
+    console.log(`Port ${PORT} is available. Starting server on this port.`);
+  } else {
+    console.warn(`Port ${PORT} is in use. Server starting on alternative port ${availablePort}.`);
+  }
+
+  app.listen(availablePort, () => {
+    console.log(`Server is running on port ${availablePort}`);
+  });
 });
