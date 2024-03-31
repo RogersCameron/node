0a3003549a262399e6275484657b165fdb379cb1
RogersCameron <156271172+RogersCameron@users.noreply.github.com>
2024-03-31 17:32:18 -0400

    t

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
 app.use(express.static("public"));
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
